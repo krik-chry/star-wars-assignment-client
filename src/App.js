d0111f8d9e1287732ad4412a70f6107c02b76f7a
krik-chry <krikonis.chrisostomos@gmail.com>
2020-01-14 18:43:25 +0100

    Images, background, title styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import './App.css';
 import MainPageContainer from './components/MainPageContainer';
+import image from './images/sw-height-img.jpg';
 
 function App() {
   return (
     <div className="App">
+      <h1 id="header">STAR WARS Height-O-Meter</h1>
+      <img alt="height-sw" src={image} width="60%" />
       <MainPageContainer />
     </div>
   );
